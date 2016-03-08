5e6706f587ea3a2d005e5a8cc4c3815366a6fec9
Jeremy Hartmann <jeremy.hartmann@gmail.com>
2016-03-07 22:03:09 -0800

    Add: initial body of sever/client in main()

--- main.cpp
+++ main.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 using namespace std;
 
+
+
+
+
+
+
+
+
+
+
+
+
 int main(int argc, char *argv[])
 {
+    cout << INADDR_ANY << endl;
      cout << "Simple Client Server" << endl;
+    int type = -1;
+    string strType;
+    if (argc <= 1)
+    {
+        cout << "Please enter parameters:\nClient=1//Server=0: ";
+        cin >> strType;
+        type = atoi(strType.c_str());
+    }
+    else
+    {
+        // Assume convertible to int. TODO: error cases.
+        type = atoi(argv[1]);
+    }
 
+    if (type > 1) return 1;
+
+    if (type)
+    {
+        // Call client
+
+    }
+    else
+    {
+        // Call server
+
+    }
 
     return 0;
 }
