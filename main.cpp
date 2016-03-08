c66d6cc569524dedc7dcc53b5dc32603ebc507b0
Jeremy Hartmann <jeremy.hartmann@gmail.com>
2016-03-07 21:20:37 -0800

    INIT: Simple Client Server Linux.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+     cout << "Simple Client Server" << endl;
+
+
+    return 0;
+}
